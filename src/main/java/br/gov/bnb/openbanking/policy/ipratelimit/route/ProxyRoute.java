9a742b1b23e023dcabf0430450ac844a5441562d
Natanael Ramalho <nramalho@redhat.com>
2020-11-12 18:17:04 -0300

    introdução de ssl para o redirect

--- src/main/java/br/gov/bnb/openbanking/policy/ipratelimit/route/ProxyRoute.java
+++ src/main/java/br/gov/bnb/openbanking/policy/ipratelimit/route/ProxyRoute.java
@@ -1,14 +1,27 @@
 package br.gov.bnb.openbanking.policy.ipratelimit.route;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
+import java.security.KeyManagementException;
 import java.util.Iterator;
 import java.util.logging.Logger;
+
+import javax.net.ssl.SSLContext;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.RouteDefinition;
+import org.apache.camel.util.jsse.KeyManagersParameters;
+import org.apache.camel.util.jsse.KeyStoreParameters;
+import org.apache.camel.util.jsse.SSLContextParameters;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
 import br.gov.bnb.openbanking.policy.ipratelimit.exception.RateLimitException;
 
 @Component("ip-rate-limit")
@@ -21,7 +34,7 @@ public class ProxyRoute extends RouteBuilder {
 		final RouteDefinition from;
 		// from = from("jetty://http://0.0.0.0:8080?useXForwardedForHeader=true&matchOnUriPrefix=true")
 		// from = from("jetty://https://0.0.0.0:8443?useXForwardedForHeader=true&matchOnUriPrefix=true");
-		from = from("netty4-http:proxy://0.0.0.0:8080?ssl=true");
+		from = from("netty4-http:proxy://0.0.0.0:8080");
 
 		from
 		.doTry()
@@ -32,18 +45,19 @@ public class ProxyRoute extends RouteBuilder {
 				.process((e) -> {
 					System.out.println(">>> forwarding request to backend");
 				})
-			.toD("http4://"
+			.toD("netty4-http:"
+			    + "${header." + Exchange.HTTP_SCHEME + "}://"
 				+ "${header." + Exchange.HTTP_HOST + "}:"
 				+ "${header." + Exchange.HTTP_PORT + "}"
 				+ "${header." + Exchange.HTTP_PATH + "}"
-				+ "?connectionClose=false&bridgeEndpoint=true&copyHeaders=true"
+				+ "?ssl=true&sslContextParameters=#sslContextParameters"
 			)
 			.process((e) -> {
 				System.out.println(">>> afterRedirect method");
 			})
             .process(ProxyRoute::afterRedirect)
-		  .endDoTry()
-          .doCatch(RateLimitException.class)
+		.endDoTry()
+        	.doCatch(RateLimitException.class)
 			.process((e) -> {
 				System.out.println(">>> afterRedirect method");
 			})
@@ -83,10 +97,12 @@ public class ProxyRoute extends RouteBuilder {
 			String host = (String) message.getHeader("CamelHttpHost");
 			String path = (String) message.getHeader("CamelHttpPath");
 			Integer port = (Integer) message.getHeader("CamelHttpPort");
+			String scheme = (String) message.getHeader("CamelHttpScheme");
 
 			LOGGER.info("REDIRECTING TO HTTP_HOST: " + host);
 			LOGGER.info("REDIRECTING TO HTTP_PORT: " + port);
 			LOGGER.info("REDIRECTING TO HTTP_PATH: " + path);
+			LOGGER.info("REDIRECTING TO HTTP_SCHEME: " + scheme);
 
 			if (host.indexOf(':') > -1) {
 				LOGGER.info("\ttrimming port from host variable: " + host);
@@ -97,9 +113,11 @@ public class ProxyRoute extends RouteBuilder {
 			message.setHeader(Exchange.HTTP_HOST, host);
 			message.setHeader(Exchange.HTTP_PORT, port);
 			message.setHeader(Exchange.HTTP_PATH, path);
+			message.setHeader(Exchange.HTTP_SCHEME, scheme);
 
 			LOGGER.info("--------------------------------------------------------------------------------");
 			LOGGER.info("PROXY FORWARDING TO "
+					+ message.getHeader(Exchange.HTTP_SCHEME)
 					+ message.getHeader(Exchange.HTTP_HOST)
 					+ ":" + message.getHeader(Exchange.HTTP_PORT)
 					+ message.getHeader(Exchange.HTTP_PATH));
@@ -123,4 +141,25 @@ public class ProxyRoute extends RouteBuilder {
     	message.setHeader(Exchange.HTTP_RESPONSE_CODE,429);
     }
 
+	@Bean(name = "sslContextParameters")
+	public SSLContextParameters sslParameters() throws KeyManagementException, GeneralSecurityException, IOException {
+
+		KeyStoreParameters ksp = new KeyStoreParameters();
+		ksp.setResource(this.getClass().getProtectionDomain().getCodeSource().getLocation()+"keystore/truststore-postman.jks");
+		ksp.setPassword("password");
+
+		KeyManagersParameters kmp = new KeyManagersParameters();
+		kmp.setKeyStore(ksp);
+		kmp.setKeyPassword("password");
+
+		SSLContextParameters scp = new SSLContextParameters();
+		scp.setKeyManagers(kmp);
+
+		SSLContextBuilder builder = new SSLContextBuilder();
+		builder.loadTrustMaterial(new TrustSelfSignedStrategy());
+		SSLContext sslcontext = builder.build();
+		scp.createSSLContext().setDefault(sslcontext);
+
+		return scp;
+	}
 }
