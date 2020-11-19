13e45e68a75963def63053961b6ad0affe49d4c3
Natanael Ramalho <nramalho@redhat.com>
2020-11-19 12:28:05 -0300

    inclusao de certificado

--- src/main/java/br/gov/bnb/openbanking/policy/ipratelimit/route/ProxyRoute.java
+++ src/main/java/br/gov/bnb/openbanking/policy/ipratelimit/route/ProxyRoute.java
@@ -9,61 +9,69 @@ import java.util.logging.Logger;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http4.HttpComponent;
+import org.apache.camel.util.jsse.KeyStoreParameters;
+import org.apache.camel.util.jsse.SSLContextParameters;
+import org.apache.camel.util.jsse.TrustManagersParameters;
 import org.springframework.stereotype.Component;
-
 
 @Component
 public class ProxyRoute extends RouteBuilder {
 	private static final Logger LOGGER = Logger.getLogger(ProxyRoute.class.getName());
 
-    @Override
-    public void configure() throws Exception {
-		
-		from("netty4-http:proxy://0.0.0.0:8080/?bridgeEndpoint=true&throwExceptionOnFailure=false")
-			.process((e) -> {
-				System.out.println("\n:: proxy received\n");
-			})
+	@Override
+	public void configure() throws Exception {
+		configureHttp4();
 
-			.to("direct:internal-redirect")
-			.process((e) -> {
-				System.out.println("\n:: route processing ended\n");
-			});
-		
-		from("direct:internal-redirect")
-			.process((e) -> {
-				System.out.println("\n:: internal-rest received\n");
-			})
-			.process(ProxyRoute::beforeRedirect)
-			.toD("https4://"
-                + "${headers." + Exchange.HTTP_HOST + "}:"
-				+ "${headers." + Exchange.HTTP_PORT + "}"
-				+ "?bridgeEndpoint=true&throwExceptionOnFailure=false")
-			.process(ProxyRoute::uppercase)
-			.process((e) -> {
-				System.out.println(":: request forwarded to backend");
-		});		
+		from("netty4-http:proxy://0.0.0.0:8080/?bridgeEndpoint=true&throwExceptionOnFailure=false").process((e) -> {
+			System.out.println("\n:: proxy received\n");
+		})
+
+				.to("direct:internal-redirect").process((e) -> {
+					System.out.println("\n:: route processing ended\n");
+				});
+
+		from("direct:internal-redirect").process((e) -> {
+			System.out.println("\n:: internal-rest received\n");
+		}).process(ProxyRoute::beforeRedirect)
+				.toD("https4://" + "${headers." + Exchange.HTTP_HOST + "}:" + "${headers." + Exchange.HTTP_PORT + "}"
+						+ "?bridgeEndpoint=true&throwExceptionOnFailure=false")
+				.process(ProxyRoute::uppercase).process((e) -> {
+					System.out.println(":: request forwarded to backend");
+				});
 	}
-	
+
+	private void configureHttp4() {
+		KeyStoreParameters ksp = new KeyStoreParameters();
+		ksp.setResource(this.getClass().getProtectionDomain().getCodeSource().getLocation()+"keystore/bnb.jks");
+		ksp.setPassword("changeit");
+		TrustManagersParameters tmp = new TrustManagersParameters();
+		tmp.setKeyStore(ksp);
+		SSLContextParameters scp = new SSLContextParameters();
+		scp.setTrustManagers(tmp);
+		HttpComponent httpComponent = getContext().getComponent("https4", HttpComponent.class);
+		httpComponent.setSslContextParameters(scp);
+	}
 
 	public static void uppercase(final Exchange exchange) {
 		final Message message = exchange.getIn();
 		final String body = message.getBody(String.class);
 		message.setBody(body.toUpperCase(Locale.US));
 	}
-		
+
 	private static void beforeRedirect(final Exchange exchange) {
 		LOGGER.info("BEFORE REDIRECT");
 		final Message message = exchange.getIn();
 		Iterator<String> iName = message.getHeaders().keySet().iterator();
 
 		LOGGER.info("header values:");
-		while(iName.hasNext()) {
+		while (iName.hasNext()) {
 			String key = (String) iName.next();
-			LOGGER.info("\t[" +key+ "] - {"+message.getHeader(key)+"}");
+			LOGGER.info("\t[" + key + "] - {" + message.getHeader(key) + "}");
 		}
 
 		// HttpServletRequest req = exchange.getIn().getBody(HttpServletRequest.class);
-		InetSocketAddress remoteAddress = (InetSocketAddress)message.getHeader("CamelNettyRemoteAddress");
+		InetSocketAddress remoteAddress = (InetSocketAddress) message.getHeader("CamelNettyRemoteAddress");
 
 		LOGGER.info("");
 		LOGGER.info("REQUEST REMOTE ADDRESS: " + remoteAddress.toString());
@@ -77,7 +85,7 @@ public class ProxyRoute extends RouteBuilder {
 		// boolean isCanAccess = CacheRepository.isCanAccess(req.getRemoteAddr());
 		// if(isCanAccess) {
 
-		if(true) {
+		if (true) {
 			LOGGER.info("");
 
 			String host = (String) message.getHeader("CamelHttpHost");
@@ -91,18 +99,14 @@ public class ProxyRoute extends RouteBuilder {
 			LOGGER.info("REDIRECTING TO HTTP_SCHEME: " + scheme);
 
 			/*
-			if (host.indexOf(':') > -1) {
-				LOGGER.info("\ttrimming port from host variable: " + host);
-				host = 	remoteAddress.getHostName();
-				LOGGER.info("\tadjusted to: " + host);
-			}
-			*/
+			 * if (host.indexOf(':') > -1) {
+			 * LOGGER.info("\ttrimming port from host variable: " + host); host =
+			 * remoteAddress.getHostName(); LOGGER.info("\tadjusted to: " + host); }
+			 */
 
 			LOGGER.info("--------------------------------------------------------------------------------");
-			LOGGER.info("PROXY FORWARDING TO "
-					+ message.getHeader(Exchange.HTTP_SCHEME)
-					+ message.getHeader(Exchange.HTTP_HOST)
-					+ ":" + message.getHeader(Exchange.HTTP_PORT)
+			LOGGER.info("PROXY FORWARDING TO " + message.getHeader(Exchange.HTTP_SCHEME)
+					+ message.getHeader(Exchange.HTTP_HOST) + ":" + message.getHeader(Exchange.HTTP_PORT)
 					+ message.getHeader(Exchange.HTTP_PATH));
 			LOGGER.info("--------------------------------------------------------------------------------");
 		}
